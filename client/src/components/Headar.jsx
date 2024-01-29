537e5ed81f6dc535cb43f297dcdfa48d6997ce01
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-29 19:23:22 +0700

    add header

---
+++ client/src/components/Headar.jsx
@@ -0,0 +1,31 @@
+const Headar = () => {
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <a href="https://flowbite.com" className="flex items-center">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/ios-filled/50/redux.png"
+              alt="redux"
+            />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Authentication system
+            </span>
+          </a>
+          <div className="flex items-center lg:order-2">
+            <a
+              href="#"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Logout
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Headar
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Headar from '../components/Headar'
 
 const HomePage = () => {
-  return <div className="text-3xl font-bold">HomePage</div>
+  return (
+    <>
+      <Headar />
+      <div className="text-3xl font-bold text-center mt-24">Hello World</div>
+    </>
+  )
 }
 
 export default HomePage
