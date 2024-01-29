64cd51208fb93d5f9c03030c52ceaefb50ae43d5
fikiaprian23 <fikiaprian23@gmail.com>
2024-01-29 19:51:08 +0700

    add user atoms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import { RegisterPage } from './pages/RegisterPage'
 import ForgotPassword from './pages/ForgotPassword'
+import { useRecoilValue } from 'recoil'
+import userAtom from './atoms/userAtom'
 
 function App() {
+  const user = useRecoilValue(userAtom)
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={user ? <HomePage /> : <LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
